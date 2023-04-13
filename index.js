5b975a36281e2fd726eab73016bb9d5698c59d52
Hoang Anh <hoanganh81097@gmail.com>
2023-04-13 21:30:29 +0700

    ok

--- index.js
+++ index.js
@@ -1,42 +1,92 @@
-let objectList = [ 
+let projectList = [ 
     {
         title : 'Caculator',
         desc : 'Mô phỏng ứng dụng Caculator trong máy tính',
         url : '/project/caculator/index.html',
-        icon : 'fa-solid fa-calculator icon-wrap'
+        icon : 'fa-solid fa-calculator'
     },
     
     {
         title : 'If, Else',
         desc : 'Cấu trúc điều kiện',
         url : 'project/if condition 1/index.html',
-        icon : 'fa-brands fa-usb icon-wrap'
+        icon : 'fa-brands fa-usb'
     },
 
     {
         title : 'For, While',
         desc : 'Cấu trúc For - While',
         url : '/project/for-while 1/index.html',
-        icon : 'fa-solid fa-rotate icon-wrap'
+        icon : 'fa-solid fa-rotate'
     },
 ]
 
-function renderList(){
-    let listWrap = document.getElementsByClassName("card-wrap")[0];
-    for (let i = 0; i < objectList.length; i++) {
-        let { icon, title, desc, url } = objectList[i];
+function renderItem(){
+    let itemWrap = document.getElementsByClassName("card-wrap")[0];
+    for (let i = 0; i < projectList.length; i++) {
+        let { icon, title, desc, url } = projectList[i];
         let cardHtml = 
             `<div class="card">
-                <i class="${icon}"></i>
+                <div class="img-wrap">
+                    <i class="${icon}"></i>
+                </div>
                 <div class="card-body">
                     <h5 class="card-title">${title}</h5>
                     <p class="card-text">${desc}</p>
                     <a href="${url}" class="btn btn-primary button-test">Xem</a>
                 </div>
             </div>`;
-        listWrap.innerHTML += cardHtml;
+        itemWrap.innerHTML += cardHtml;
     }
 }
 
-renderList();
+// for (const item of projectList) {
+    //     itemWrap.innerHTML += `
+    //         <div class="card">
+    //             <div class="img-wrap">
+    //                 <i class="${item.icon}"></i>
+    //             </div>
+    //             <div class="card-body">
+    //                 <h5 class="card-title">${item.title}</h5>
+    //                 <p class="card-text">${item.desc}</p>
+    //                 <a href="${item.url}" class="btn btn-primary">Xem</a>
+    //             </div>
+    //         </div>
+    //     `
+    // }
+
+    // projectList.forEach(item => {
+    //     itemWrap.innerHTML += `
+    //         <div class="card">
+    //             <div class="img-wrap">
+    //                 <i class="${item.icon}"></i>
+    //             </div>
+    //             <div class="card-body">
+    //                 <h5 class="card-title">${item.title}</h5>
+    //                 <p class="card-text">${item.desc}</p>
+    //                 <a href="${item.url}" class="btn btn-primary">Xem</a>
+    //             </div>
+    //         </div>
+    //     `
+    // });
+
+    // let index = 0;
+    // while (index < projectList.length) {
+    //     let item = projectList[index];
+    //     itemWrap.innerHTML += `
+    //         <div class="card">
+    //             <div class="img-wrap">
+    //                 <i class="${item.icon}"></i>
+    //             </div>
+    //             <div class="card-body">
+    //                 <h5 class="card-title">${item.title}</h5>
+    //                 <p class="card-text">${item.desc}</p>
+    //                 <a href="${item.url}" class="btn btn-primary">Xem</a>
+    //             </div>
+    //         </div>
+    //     `
+    //     index++;     
+    // }
+
+renderItem();
 
