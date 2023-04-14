872eb7a90611975c410aa419dc1b86182b9a8178
Cass81097 <truonghoanganh.810@gmail.com>
2023-04-14 09:08:39 +0700

    change cons -> func

--- project/anim/index.js
+++ project/anim/index.js
@@ -1,11 +1,11 @@
 const ball = document.querySelector('.ball');
 
-const startAnimation = () => {
-ball.style.animationPlayState = 'running';
+function startAnimation(){
+    ball.style.animationPlayState = 'running';
 };
 
-const stopAnimation = () => {
-ball.style.animationPlayState = 'paused';
+function stopAnimation(){
+    ball.style.animationPlayState = 'paused';
 };
 
 // document.getElementById('startBall').addEventListener('click', startAnimation);
